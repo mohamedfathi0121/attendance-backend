ff97f4475ae68d4b3f694e573cf7e0d165efed12
Mohamed Fathi Salah Hassan <mohamed.fathi.sallah@commerce.helwan.edu.eg>
2026-04-28 09:44:41 +0300

    hello world

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes/studentRoutes');
+const routes = require("./routes/studentRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.listen(process.env.PORT, () => {
-  console.log('Server running...');
+  console.log("Server running...");
 });
